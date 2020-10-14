217e0339d3b6c2b24c28c6635fa0db409cbbf477
KevenTom <zpqls1648@naver.com>
2020-10-14 23:16:36 +0900

    양방향 연결 리스트 추가

---
+++ Data Structures/List/Project1/소스.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Point {
+private:
+    double m_x;
+    double m_y;
+    double m_z;
+public:
+    Point(double x = 0.0, double y = 0.0, double z = 0.0) : m_x(x), m_y(y), m_z(z) {}
+
+    friend std::ostream& operator<< (std::ostream& out, const Point& point);
+};
+
+std::ostream& operator<< (std::ostream& out, const Point& point) {
+    out << "Point(" << point.m_x << ", " << point.m_y << ", " << point.m_z << ")";
+
+    return out;
+}
+
+int main()
+{
+    Point point1(2.0, 3.0, 4.0);
+    std::cout << point1;
+
+    return 0;
+}
+/*
+Point(2, 3, 4)
