988a14c1dd046132a6a46eeb8e099d43f9bf141b
KevenTom <zpqls1648@naver.com>
2020-10-21 11:39:37 +0900

    소멸자 수정

---
+++ Data Structures/Stack, Queue, Deque/Stack/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include"Stack.h"
+
+using namespace std;
+
+int main() {
+	while (true) {
+		Stack<int> stack;
+
+		stack.SPush(1);
+		stack.SPush(2);
+		stack.SPush(3);
+		stack.SPush(4);
+		stack.SPush(5);
+	}
+
+
+	//while (!stack.SIsEmpty()) {
+	//	cout << stack.SPeek() << " ";
+	//	stack.SPop();
+	//}
+
+
+
+	return 0;
+}
