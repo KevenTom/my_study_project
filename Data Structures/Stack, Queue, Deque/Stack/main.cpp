69a2928a780013eecf312036a5a3ccb7c4441143
KevenTom <zpqls1648@naver.com>
2020-10-21 11:57:57 +0900

    이름 수정

--- Data Structures/Stack, Queue, Deque/Stack/main.cpp
+++ Data Structures/Stack, Queue, Deque/Stack/main.cpp
@@ -4,21 +4,18 @@
 using namespace std;
 
 int main() {
-	while (true) {
-		Stack<int> stack;
+	Stack<int> stack;
 
-		stack.SPush(1);
-		stack.SPush(2);
-		stack.SPush(3);
-		stack.SPush(4);
-		stack.SPush(5);
+	stack.SPush(1);
+	stack.SPush(2);
+	stack.SPush(3);
+	stack.SPush(4);
+	stack.SPush(5);
+
+	while (!stack.SIsEmpty()) {
+		cout << stack.SPeek() << " ";
+		stack.SPop();
 	}
-
-
-	//while (!stack.SIsEmpty()) {
-	//	cout << stack.SPeek() << " ";
-	//	stack.SPop();
-	//}
 
 
 
